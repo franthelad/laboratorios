423dc008afb6453a7535c0b91e050f5540b94ebf
franthelad <francisco.jesus.pastor@gmail.com>
2024-01-23 12:53:36 +0100

    laboratorios Edube sobre clases con enunciado

---
+++ laboratorio3Edube_clases.py
@@ -0,0 +1,40 @@
+'''
+Your task is to slightly extend the Queue class' capabilities. We want it to
+have a parameterless method that returns True if the queue is empty and False
+otherwise.
+'''
+
+class QueueError(IndexError):
+    pass
+
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+    def put(self,elem):
+        self.queue.insert(0,elem)
+    def get(self):
+        if len(self.queue) > 0:
+            elem = self.queue[-1]
+            del self.queue[-1]
+            return elem
+        else:
+            raise QueueError
+        
+class SuperQueue(Queue):
+    def __init__(self):
+        self.queue = []
+        
+    def isempty(self):
+        return len(self.queue) == 0
+
+que = SuperQueue()
+que.put(1)
+que.put("dog")
+que.put(False)
+for i in range(4):
+    if not que.isempty():
+        print(que.get())
+    else:
+        print("Queue empty")
+
